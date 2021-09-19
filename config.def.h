9511828f9729be2b572b4be173fac5733b3d369d
Thayer Williams <thayerwilliams@gmail.com>
2021-09-19 08:55:25 -0700

    barhight patch; define bar height in user config

--- config.def.h
+++ config.def.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 24;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "IBM Plex Mono:size=10:style=bold", "BitstreamVeraSansMono Nerd Font:size=11" };
 static const char dmenufont[]       = "IBM Plex Mono:size=10:style=bold";
 static const char col_grey1[]       = "#1a1a1a";
