83dc0bba365dec704a1b6af1f6cdaec57fabcd1f
Thayer Williams <thayerwilliams@gmail.com>
2021-10-11 11:03:12 -0700

    systray patch applied using Quentin Franchi's modified tray icon size patch; https://gitlab.com/-/snippets/2184056

--- config.def.h
+++ config.def.h
@@ -3,6 +3,12 @@
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning  = 0;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft   = 0;  /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing  = 6;  /* systray spacing */
+static const unsigned int systrayiconsize = 18; /* systray icon size in px */
+static const int systraypinningfailfirst  = 1;  /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray              = 1;  /* 0 means no systray */
 static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
